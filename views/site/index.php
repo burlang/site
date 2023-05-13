705869e39354d2febc5b93704604cd25dadfae75
Bulat Damdinov <dbulats88@gmail.com>
2023-05-12 18:01:27 -0700

    Add links to apps

--- views/site/index.php
+++ views/site/index.php
@@ -14,7 +14,14 @@ $this->title = Yii::$app->name . ' - Русско-Бурятский, Бурят
     <div class="row mt-10">
         <div class="col-sm-8">
             <?= $this->render('partials/buryat_words_form') ?>
-            <br>
+            <div class="text-center mt-20">
+                <a href="https://apps.apple.com/ru/app/burlang/id1669964114" target="_blank" rel="noopener noreferrer">
+                    <img src="<?= Yii::getAlias('@web/icon/app-store.svg') ?>" alt="AppStore" height="56">
+                </a>
+                <a href="https://play.google.com/store/apps/details?id=burlang.ru"  target="_blank" rel="noopener noreferrer">
+                    <img src="<?= Yii::getAlias('@web/icon/app-store.svg') ?>" alt="GooglePlay" height="55">
+                </a>
+            </div>
             <?= Comments::widget() ?>
         </div>
         <div class="col-sm-4">
