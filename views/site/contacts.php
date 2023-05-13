4094770566cb3c4e5c87090c32a9147176b58bf8
Bulat Damdinov <dbulats88@gmail.com>
2023-05-12 21:52:25 -0700

    Add email info@burlang.org on contacts page

--- views/site/contacts.php
+++ views/site/contacts.php
@@ -14,7 +14,11 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <h1><?= $this->title ?></h1>
 
-<div class="row mt-20">
+<div class="mt-20">
+    <h5><b>Email:</b> <?= Html::mailto('info@burlang.org') ?></h5>
+</div>
+<hr>
+<div class="row">
     <div class="col-md-4">
         <h4>Булат Дамдинов</h4>
         <?= Html::mailto('bulat@damdinov.me') ?>
