8b566e377307c3852918b187f97e28f96fed8e24
Bulat Damdinov <dbulats88@gmail.com>
2023-05-12 18:04:02 -0700

    Minor fix

--- views/site/index.php
+++ views/site/index.php
@@ -14,12 +14,13 @@ $this->title = Yii::$app->name . ' - Русско-Бурятский, Бурят
     <div class="row mt-10">
         <div class="col-sm-8">
             <?= $this->render('partials/buryat_words_form') ?>
+            <hr>
             <div class="text-center mt-20">
                 <a href="https://apps.apple.com/ru/app/burlang/id1669964114" target="_blank" rel="noopener noreferrer">
-                    <img src="<?= Yii::getAlias('@web/icon/app-store.svg') ?>" alt="AppStore" height="56">
+                    <img src="<?= Yii::getAlias('@web/icon/app-store.svg') ?>" alt="AppStore" height="40">
                 </a>
                 <a href="https://play.google.com/store/apps/details?id=burlang.ru"  target="_blank" rel="noopener noreferrer">
-                    <img src="<?= Yii::getAlias('@web/icon/google-play.svg') ?>" alt="GooglePlay" height="55">
+                    <img src="<?= Yii::getAlias('@web/icon/google-play.svg') ?>" alt="GooglePlay" height="40">
                 </a>
             </div>
             <?= Comments::widget() ?>
