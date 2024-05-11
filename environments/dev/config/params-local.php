6c51c720515c57c8520559622e2fc96846ce943b
Bulat Damdinov <dbulats88@gmail.com>
2024-05-10 23:31:31 -0700

    Remove using swiftmailer in the dev env

--- environments/dev/config/params-local.php
+++ environments/dev/config/params-local.php
@@ -11,10 +11,6 @@ return [
     ],
 
     'components.mailer' => [
-        'class' => \yii\swiftmailer\Mailer::class,
-        // send all mails to a file by default. You have to set
-        // 'useFileTransport' to false and configure a transport
-        // for the mailer to send real emails.
         'useFileTransport' => true,
     ],
 ];
