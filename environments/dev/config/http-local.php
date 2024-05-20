07d483d97bab98ae3d17a5c004dc2646f0a35335
Bulat Damdinov <dbulats88@gmail.com>
2024-05-19 20:43:55 -0700

    Remove yii2-debug config

--- environments/dev/config/http-local.php
+++ environments/dev/config/http-local.php
@@ -1,15 +1,5 @@
 <?php
 
-$config = [
+return [
     'components' => [],
 ];
-
-if (!YII_ENV_TEST) {
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => \yii\debug\Module::class,
-        'allowedIPs' => ['*'],
-    ];
-}
-
-return $config;
