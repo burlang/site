621809d65d201044949558e6e3e7b069154ba32d
Bulat Damdinov <dbulats88@gmail.com>
2024-05-24 23:24:35 -0700

    Add "database" directory

--- config/console.php
+++ config/console.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use yii\console\controllers\MigrateController;
 use yii\console\ErrorHandler;
 use yii\helpers\ArrayHelper;
 
@@ -9,6 +10,14 @@ return ArrayHelper::merge(
     require __DIR__ . '/common.php',
     [
         'controllerNamespace' => 'app\commands',
+        'controllerMap' => [
+            'migrate' => [
+                'class' => MigrateController::class,
+                'migrationPath' => [
+                    '@root/database/migrations',
+                ],
+            ],
+        ],
         'components' => [
             'errorHandler' => [
                 'class' => ErrorHandler::class,
