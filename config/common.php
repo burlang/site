ce16876000be56752f55303c9beb16d2d0b01a01
Bulat Damdinov <dbulats88@gmail.com>
2024-05-27 01:12:49 -0700

    Rename MYSQL_* environment variables

--- config/common.php
+++ config/common.php
@@ -66,9 +66,9 @@ return [
             ],
             Connection::class => [
                 'class' => Connection::class,
-                'dsn' => 'mysql:host=' . env('DB_HOST') . ';dbname=' . env('DB_DATABASE'),
-                'username' => env('DB_USERNAME'),
-                'password' => env('DB_PASSWORD'),
+                'dsn' => 'mysql:host=' . env('MYSQL_HOST') . ';dbname=' . env('MYSQL_DATABASE'),
+                'username' => env('MYSQL_USER'),
+                'password' => env('MYSQL_PASSWORD'),
                 'charset' => 'utf8',
                 'enableSchemaCache' => true,
             ],
