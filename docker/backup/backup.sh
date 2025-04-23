ff12dfadb53dc372389113f06b943b1898fccfba
Bulat Damdinov <dbulats88@gmail.com>
2025-04-22 18:27:35 -0700

    Update backup script to use mariadb-dump and add SSL option

--- docker/backup/backup.sh
+++ docker/backup/backup.sh
@@ -7,8 +7,8 @@ BACKUP_FILE="${BACKUP_NAME:?}_$(date +%Y-%m-%d_%H-%M).sql.gz"
 
 echo "Dump $BACKUP_FILE"
 
-mysqldump --host="${MYSQL_HOST:?}" --user="${MYSQL_USER:?}" --password="${MYSQL_PASSWORD:?}" \
-  --single-transaction "${MYSQL_DATABASE:?}" | gzip -9 > "$BACKUP_FILE"
+mariadb-dump --host="${MYSQL_HOST:?}" --user="${MYSQL_USER:?}" --password="${MYSQL_PASSWORD:?}" \
+  --ssl=0 --single-transaction "${MYSQL_DATABASE:?}" | gzip -9 > "$BACKUP_FILE"
 
 echo "Upload to S3"
 
@@ -19,3 +19,5 @@ export AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION:?}"
 aws --endpoint-url="${S3_ENDPOINT:?}" s3 cp "$BACKUP_FILE" "s3://${S3_BUCKET:?}/$BACKUP_FILE"
 
 unlink "$BACKUP_FILE"
+
+echo -e "\e[32m✔ Backup completed successfully!\e[0m"
